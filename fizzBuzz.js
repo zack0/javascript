b41cae44ee037301264917b0dce5d8bfc5ba8677
zack0 <zack.timmer@gmail.com>
2015-11-24 08:41:46 -0700

    FizzBuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,18 +1,21 @@
-/*
 
+
+// if else else if
+/*
 for (i = 0; i <= 100; i++) {
 	if (i % 3 === 0 && i % 5 != 0) {
-		console.log('Fizz ' + i + ' is divisible by 3 but not by 5');
+		console.log('Fizz');
 	} else if (i % 5 === 0 && i % 3 != 0) {
-		console.log('Buzz ' + i + ' is divisible by 5 but not by 3');
+		console.log('Buzz');
 	} else if (i % 3 === 0 && i % 5 === 0) {
-		console.log('FizzBuzz ' + i + ' is divisible by both 3 and 5');
+		console.log('FizzBuzz');
 	}
 	else console.log(i);
 }
-
 */
 
+// while ternary
+// /*
 var i = 0;
 while (i <= 100) {
 	i % 3 === 0 && i % 5 != 0 ? console.log('Fizz') 
@@ -21,3 +24,4 @@ while (i <= 100) {
 	: console.log(i);
 	i++;
 }
+// */
