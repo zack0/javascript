e2ab4569b76b8a97e4ec3b0d3a41fca2fafccff6
zack0 <zack.timmer@gmail.com>
2015-11-24 10:29:24 -0700

    min

---
+++ min.js
@@ -0,0 +1,8 @@
+function min(one, two) {
+	if (one > two) {
+		return one;
+	} else if (one === two) {
+		return one + ' and ' + two + ' are equal';
+	} else return two;
+}
+console.log(min(10, 10));
